4dd95ce4e4518a468c0d6a26cac22c20f95c4e0e
SeckinBakir <seckin.bakir07@gmail.com>
2021-02-28 20:40:27 +0300

    First Commit
    First Commit

---
+++ Çalışanlar/calisanlar.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 06 Ara 2020, 13:42:07
+-- Sunucu sürümü: 10.4.14-MariaDB
+-- PHP Sürümü: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `demo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `calisanlar`
+--
+
+CREATE TABLE `calisanlar` (
+  `id` int(11) NOT NULL,
+  `ad` text COLLATE utf8mb4_turkish_ci NOT NULL,
+  `soyad` text COLLATE utf8mb4_turkish_ci NOT NULL,
+  `email` text COLLATE utf8mb4_turkish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_turkish_ci;
+
+--
+-- Tablo döküm verisi `calisanlar`
+--
+
+INSERT INTO `calisanlar` (`id`, `ad`, `soyad`, `email`) VALUES
+(1, 'Seçkin Emre', 'Bakır', 'seckin-bakir@outlook.com'),
+(2, 'mehmet', 'gencol', 'blabla@outlook.com'),
+(3, 'cemre', 'yagmur', 'cemre@gmail.com'),
+(4, 'sekobuba', 'bubaseko', 'bubaseko@gmail.com'),
+(5, 'Yusuf', 'Kalkan', 'yusuf@gmail.com');
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `calisanlar`
+--
+ALTER TABLE `calisanlar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `calisanlar`
+--
+ALTER TABLE `calisanlar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
